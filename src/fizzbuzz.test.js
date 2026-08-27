d81773c76cf9e7eb3745480625698fd2d22ab88c
Maite Arauco <gabriela.arauco.p@ucb.edu.bo>
2026-08-27 10:00:50 -0400

    feat: Generar fizzbuzz para numeros multiplos de 3 y 5

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,34 +4,37 @@ describe("calcularFizzBuzz", () => {
   it("Si el numero no es multiplo de 3 ni de 5 deberia devolver el mismo numero", () => {
     expect(calcularFizzBuzz(1)).toEqual("1");
   });
-});
 
-describe("calcularFizzBuzz", () => {
   it("Si el numero no es multiplo de 3 ni de 5 deberia devolver el mismo numero", () => {
     expect(calcularFizzBuzz(2)).toEqual("2");
   });
-});
 
-describe("calcularFizzBuzz", () => {
   it("Si el numero es multiplo de 3 deberia devolver Fizz", () => {
     expect(calcularFizzBuzz(3)).toEqual("Fizz");
   });
-});
 
-describe("calcularFizzBuzz", () => {
   it("Si el numero es multiplo de 3 deberia devolver Fizz", () => {
     expect(calcularFizzBuzz(6)).toEqual("Fizz");
   });
-});
 
-describe("calcularFizzBuzz", () => {
   it("Si el numero es multiplo de 5 deberia devolver Buzz", () => {
     expect(calcularFizzBuzz(5)).toEqual("Buzz");
   });
-});
 
-describe("calcularFizzBuzz", () => {
   it("Si el numero es multiplo de 5 deberia devolver Buzz", () => {
     expect(calcularFizzBuzz(10)).toEqual("Buzz");
   });
+
+  it("Si el numero es multiplo de 5 deberia devolver Buzz", () => {
+    expect(calcularFizzBuzz(10)).toEqual("Buzz");
+  });
+
+  it("Si el numero es multiplo de 3 y de 5 deberia devolver FizzBuzz", () => {
+    expect(calcularFizzBuzz(15)).toEqual("FizzBuzz");
+  });
+
+  it("Si el numero es multiplo de 3 y de 5 deberia devolver FizzBuzz", () => {
+    expect(calcularFizzBuzz(30)).toEqual("FizzBuzz");
+  });
+
 });
